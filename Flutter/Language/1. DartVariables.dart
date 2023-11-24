6f32422b64ee842a69ea3a50f44b8d608e60b005
Documentation Duh :3 <96073081+dr-croc@users.noreply.github.com>
2023-11-24 19:17:28 +0600

    Create 1. DartVariables.dart

---
+++ Flutter/Language/1. DartVariables.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  //string
+  var name = "hussain";
+  print(name);
+
+  String name_2 = "Mustafa";
+  print(name_2);
+
+  // final type
+  final num = 1;
+  const num1 = 2;
+
+  // null check variable
+  String? name_3; // if name assigned then ok, other wise null
+
+  // late varable
+  late String name_4; // will be populated the value
+
+  print(name_3);
+
+}
