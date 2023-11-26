0fdf593088e19a27fefd0479ee55e4fb9c46f775
Documentation Duh :3 <96073081+dr-croc@users.noreply.github.com>
2023-11-26 13:13:33 +0600

    Create 4. Map.dart

---
+++ Flutter/Language/4. Map.dart
@@ -0,0 +1,12 @@
+ var cars = {
+    "tesla" : "electric",
+    "toyota" : "Gasolin"
+  };
+
+
+var fruits  = new Map();
+fruits["apple"] = "Red";
+print(fruits);
+
+  // acccess the values
+print(fruits["apple"]); // Red
